402f375e9e891a6b4341d115f40fdc5dc15fa27d
davissantana-byte <davissantana2007@gmail.com>
2025-10-25 18:03:13 -0300

    Adicionando scroll

---
+++ js/main.js
@@ -0,0 +1,28 @@
+const scrollContainer = document.querySelector('.grupo-caneca');
+
+let isDown = false;
+let startX;
+let scrollLeft;
+
+scrollContainer.addEventListener('mousedown', (e) => {
+  isDown = true;
+  scrollContainer.classList.add('active');
+  startX = e.pageX - scrollContainer.offsetLeft;
+  scrollLeft = scrollContainer.scrollLeft;
+});
+
+scrollContainer.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+scrollContainer.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+scrollContainer.addEventListener('mousemove', (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - scrollContainer.offsetLeft;
+  const walk = (x - startX) * 2; // multiplica pra ajustar a velocidade
+  scrollContainer.scrollLeft = scrollLeft - walk;
+});
